d073f09e096afbd60cbfeb528c5753e4310ce078
Risa Yamamoto <yamari2422@gmail.com>
2024-10-04 16:25:06 -0700

    Finished exercise10

--- dist/ex10.js
+++ dist/ex10.js
@@ -6,5 +6,6 @@ function createUser(name, age) {
     return { name, age };
 }
 const myCreateUser = function (name, age) {
+    return { name, age };
 };
 console.log(myCreateUser("John", 25)); // Expected output: { name: "John", age: 25 }
